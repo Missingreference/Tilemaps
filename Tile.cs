f98b10b8cac30c8743ae928d699f73fc2e0b7a2c
Isaac <IsaacPerron@hotmail.com>
2022-04-23 20:22:53 -0400

    Simplified Tile Class

--- Tile.cs
+++ Tile.cs
@@ -14,45 +14,9 @@ namespace Elanetic.Tilemaps
     public abstract class Tile
     {
         //The position of the tile on the tilemap. No two tiles share the same position
-        public Vector2Int position { get; internal set; }
+        public Vector2Int position { get; private set; }
         //The tilemap that this tile belongs to
-        public Tilemap tilemap { get; internal set; }
-
-        public int chunkIndex { get; private set; }
-        public int tileIndex { get; private set; }
-
-        public int tilemapTextureIndex { get; internal set; } = 0;
-
-        public RenderType renderType
-        {
-            get => m_RenderType;
-            set
-            {
-                if(m_RenderType == value) return;
-                m_RenderType = value;
-                if(m_RenderType == RenderType.Individual)
-                {
-#if SAFE_EXECUTION
-                    m_IndivualRenderer = new GameObject("Individual Tile (" + position.x + ", " + position.y + ")").AddComponent<SpriteRenderer>();
-#else
-                    m_IndivualRenderer = new GameObject("Individual Tile").AddComponent<SpriteRenderer>();
-#endif
-                    m_IndivualRenderer.transform.SetParent(tilemap.transform);
-                }
-                else if(m_RenderType == RenderType.Group)
-                {
-
-                }
-                else //RenderType.Chunk
-                {
-                    //TODO Destroy Individual/Group mesh parts
-                    tilemap.SetCellTexture(position, tilemapTextureIndex);
-                }
-            }
-        }
-
-        private RenderType m_RenderType = RenderType.Chunk;
-        private SpriteRenderer m_IndivualRenderer;
+        public Tilemap tilemap { get; private set; }
 
         protected Tile(Tilemap tilemap, int positionX, int positionY)
         {
@@ -67,29 +31,5 @@ namespace Elanetic.Tilemaps
 
         //Called by the tilemap when the tilemap has been destroyed or the cell position of this tile has been replaced by another tile or set to null
         protected virtual void OnDestroyed() { }
-
-        /// <summary>
-        /// Individual: Tile is place as it's own GameObject and SpriteRenderer. Less peformance but allows for different rendering possibilities such as individual sorting
-        /// Group: Try to group similar tiles when placed in lines or rectangles. Otherwise render individually.
-        /// Chunk: This tile is rendered with the Texture Grid class where it is rendered within a chunk's quad mesh for maximum performance
-        /// </summary>
-        public enum RenderType
-        {
-            Individual,
-            Group,
-            Chunk
-        }
-
-        /// <summary>
-        /// None: No collision
-        /// Tile: Collision encapsulates the whole tile. Is added to custom Tilemap Collider when possible for maximum performance
-        /// Custom: Collision is of a custom shape
-        /// </summary>
-        public enum CollisionType
-        {
-            None,
-            Tile,
-            Custom
-        }
     }
 }
